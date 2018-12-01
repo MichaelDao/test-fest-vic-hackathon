ca30de740b76d37e881dbd222978ffb248a3a5c9
Wilson Mun <wilsonmungit@gmail.com>
2018-12-01 16:08:48 +1100

    updated nav bar and home

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,64 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <v-app light>
+            <!--<head>-->
+                <!--<link href='https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons'-->
+                      <!--rel="stylesheet">-->
+            <!--</head>-->
+
+            <v-toolbar>
+                <v-toolbar-side-icon></v-toolbar-side-icon>
+                <router-link to="/"><img src="@/assets/logo.png"/></router-link>
+                <v-spacer></v-spacer>
+                <v-toolbar-items>
+                    <v-btn flat to="/">Home</v-btn>
+                    <v-btn flat to="/about">About</v-btn>
+                </v-toolbar-items>
+            </v-toolbar>
+
+            <router-view/>
+        </v-app>
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+    import Vue from 'vue';
+    import Vuetify from 'vuetify';
+    import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+
+    Vue.use(Vuetify);
+
+    export default {
+        name: 'App',
+        components: {},
+        data() {
+            return {
+                //
+            }
+        }
     }
-  }
-}
+
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
---
+++ src/views/Quiz.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+    import 'vuetify/dist/vuetify.min.css'; // Ensure you are using css-loader
+
+    export default {
+        name: "Quiz"
+    }
+</script>
+
+<style scoped>
+
+</style>

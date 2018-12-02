99ccaeaa03e89d50c0416564130107a57c080263
Simon Lay <s3658769@student.rmit.edu.au>
2018-12-02 12:06:06 +1100

    added images and price

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,46 +1,134 @@
 <template>
     <v-content>
         <v-container fluid>
+            <v-layout row wrap>
             <v-flex xs12 sm6 md4>
                 <v-card>
                     <!--image here-->
                     <v-img
-                            :src="require('@/assets/plane.jpg')"
+                            :src="require('@/assets/atomo01.jpg')"
                             height="200px"
                     >
                     </v-img>
 
                     <v-card-title primary-title>
                         <div>
-                            <!--title here-->
-
+                            Atomo Diagnostics HIV Testing Kit
                         </div>
                     </v-card-title>
 
                     <v-card-text>
-                        <!--content-->
-
+                        $TBA
                     </v-card-text>
 
                     <v-card-actions>
-                        <v-btn flat>Share</v-btn>
-                        <v-btn flat color="purple">Explore</v-btn>
-                        <v-spacer></v-spacer>
-                        <v-btn icon @click="show = !show">
-                            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                        </v-btn>
+                        <!--<v-btn flat>Share</v-btn>-->
+                        <v-btn style="left:42%" flat color="purple">Buy</v-btn>
+                        <!--<v-spacer></v-spacer>
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                            </v-btn>-->
                     </v-card-actions>
-
-                    <v-slide-y-transition>
-                        <v-card-text v-show="show">
-                            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have
-                            time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that
-                            data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry?
-                            I've got to find a way to escape.
-                        </v-card-text>
-                    </v-slide-y-transition>
                 </v-card>
             </v-flex>
+                <v-flex xs12 sm6 md4>
+                    <v-card>
+                        <!--image here-->
+                        <v-img
+                                :src="require('@/assets/condomBundle.jpg')"
+                                height="200px"
+                        >
+                        </v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                Bundle of Condoms
+
+                            </div>
+                        </v-card-title>
+
+                        <v-card-text>
+                            $7
+
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <!--<v-btn flat>Share</v-btn>-->
+                            <v-btn style="left:42%" flat
+                                   color="purple">Buy</v-btn>
+                            <!--<v-spacer></v-spacer>
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                            </v-btn>-->
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                    <v-card>
+                        <!--image here-->
+                        <v-img
+                                :src="require('@/assets/durex.jpeg')"
+                                height="200px"
+                        >
+                        </v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                2xPair Durex condoms
+
+                            </div>
+                        </v-card-title>
+
+                        <v-card-text>
+                            $5
+
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <!--<v-btn flat>Share</v-btn>-->
+                            <v-btn to="./BuyModel" style="left:42%" flat
+                                   color="purple">Buy
+                            </v-btn>
+                            <!--<v-spacer></v-spacer>
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                            </v-btn>-->
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+                <v-flex xs12 sm6 md4>
+                    <v-card>
+                        <!--image here-->
+                        <v-img
+                                :src="require('@/assets/femaleCondom.jpg')"
+                                height="200px"
+                        >
+                        </v-img>
+
+                        <v-card-title primary-title>
+                            <div>
+                                Female Condom
+
+                            </div>
+                        </v-card-title>
+
+                        <v-card-text>
+                           $18.50
+
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <!--<v-btn flat>Share</v-btn>-->
+                            <v-btn style="left:42%" flat color="purple">Buy
+                            </v-btn>
+                            <!--<v-spacer></v-spacer>
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                            </v-btn>-->
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
         </v-container>
     </v-content>
 </template>

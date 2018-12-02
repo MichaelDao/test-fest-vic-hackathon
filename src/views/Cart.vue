f627ea9d72ac9918ff5aab779a5a47d213ba848e
Wilson Mun <wilsonmungit@gmail.com>
2018-12-02 13:17:37 +1100

    cart in progress

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,6 +1,26 @@
 <template>
     <v-container>
-
+        <v-layout
+            row
+            wrap
+        >
+            <v-flex xs12 sm12 md12>
+            <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    class="elevation-1"
+            >
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td class="text-xs-right">{{ props.item.calories }}</td>
+                    <td class="text-xs-right">{{ props.item.fat }}</td>
+                    <td class="text-xs-right">{{ props.item.carbs }}</td>
+                    <td class="text-xs-right">{{ props.item.protein }}</td>
+                    <td class="text-xs-right">{{ props.item.iron }}</td>
+                </template>
+            </v-data-table>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
